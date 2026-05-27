a6d19c7d8d46834ed6116cdc9160b8ecd4c680cb
Albert Cruz <albertcruz@terra.com.br>
2026-05-27 11:44:00 +0100

    Atualização da barra de editorias do site

--- themes/bahia_refactor/header.php
+++ themes/bahia_refactor/header.php
@@ -126,12 +126,6 @@ require_once get_template_directory() . '/template-parts/social-icons.php';
             <a class="item" href="<?php bloginfo('url'); ?>/carnaval">
                 <p class="txtItemMenu">Carnaval</p>
             </a>
-            <a class="item" href="<?php bloginfo('url'); ?>/social">
-                <p class="txtItemMenu">Ginno Larry</p>
-            </a>
-            <a class="item" href="<?php bloginfo('url'); ?>/investimentos">
-                <p class="txtItemMenu">Investimentos</p>
-            </a>
             <!-- <a class="item" href="<?//php bloginfo('url'); ?>/eleicoes2024">
                 <p class="txtItemMenu">Eleições 2024</p>
             </a> -->
@@ -255,12 +249,6 @@ require_once get_template_directory() . '/template-parts/social-icons.php';
                             <!-- <div class="menu url-link" data-url="<?//php bloginfo('url'); ?>/eleicoes2024">
                                 <div class="item"><a href="#/">Eleições 2024</a></div>
                             </div> -->
-                        </div>
-                        <div class="item url-link" data-url="<?php bloginfo('url'); ?>/social">
-                            <a href="#/">Ginno Larry</a>
-                        </div>
-                        <div class="item url-link" data-url="<?php bloginfo('url'); ?>/investimentos">
-                            <a href="#/">Investimentos</a>
                         </div>
                         <div class="item url-link" data-url="<?php bloginfo('url'); ?>/salvador">
                             <a href="#/">Salvador</a>
@@ -454,12 +442,6 @@ require_once get_template_directory() . '/template-parts/social-icons.php';
                                                     <div class="item url-link"><a href="#/" class="linkMenu" data-url="<?php bloginfo('url'); ?>/politica-de-cookies">Política de Cookies</a></div>
                                                 </div>
                                             </div>
-                                            <div class="item url-link" data-url="<?php bloginfo('url'); ?>/social">
-                                                <a href="#/">Ginno Larry</a>
-                                            </div>
-                                            <div class="item url-link" data-url="<?php bloginfo('url'); ?>/investimentos">
-                                                <a href="#/">Investimentos</a>
-                                            </div>
                                             <div class="item url-link" data-url="<?php bloginfo('url'); ?>/salvador">
                                                 <a href="#/">Salvador</a>
                                             </div>
@@ -503,11 +485,10 @@ require_once get_template_directory() . '/template-parts/social-icons.php';
                             <li><a href="<?php bloginfo('url'); ?>/politica" data-notificacao="">Política</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/esporte" data-notificacao="">Esporte</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/entretenimento" data-notificacao="">Entretenimento</a></li>
-                            <li><a href="<?php bloginfo('url'); ?>/social" data-notificacao="">Ginno Larry</a></li>
-                            <li><a href="<?php bloginfo('url'); ?>" data-notificacao="">Investimentos</a></li>
+                            <li><a href="<?php bloginfo('url'); ?>/municipios" data-notificacao="">Municípios</a></li>
+                            <li><a href="<?php bloginfo('url'); ?>/justica" data-notificacao="">Justiça</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/salvador" data-notificacao="">Salvador</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/bahia" data-notificacao="">Bahia</a></li>
-                            <li><a href="<?php bloginfo('url'); ?>/justica" data-notificacao="">Justiça</a></li>
                         </ul>
                     </nav>
                     <nav class="nav-drop">
@@ -516,8 +497,6 @@ require_once get_template_directory() . '/template-parts/social-icons.php';
                             <li><a href="<?php bloginfo('url'); ?>/quem-somos" data-notificacao="">Quem Somos</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/carnaval" data-notificacao="">Carnaval</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/politica" data-notificacao="">Política</a></li>
-                            <li><a href="<?php bloginfo('url'); ?>/social" data-notificacao="">Ginno Larry</a></li>
-                            <li><a href="<?php bloginfo('url'); ?>" data-notificacao="">Investimentos</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/salvador" data-notificacao="">Salvador</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/bahia" data-notificacao="">Bahia</a></li>
                             <li><a href="<?php bloginfo('url'); ?>/municipios" data-notificacao="">Municípios</a></li>
