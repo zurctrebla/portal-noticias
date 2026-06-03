116b28667869a0051c3bcb5efc7912b80127d863
Albert Cruz <albertcruz@terra.com.br>
2026-06-03 21:38:07 +0100

    SCRUM-54/ Adicionar botão para nosso canal do YouTube na barra de redes sociais

--- themes/bahia_refactor/header.php
+++ themes/bahia_refactor/header.php
@@ -365,6 +365,15 @@ $ipad = strpos(getUserAgent(), "iPad");
                         </svg>
                     </a>
                 <?php endif; ?>
+                <?php
+                $youtube = get_option('options_youtube');
+                if (!$youtube) $youtube = 'https://www.youtube.com/@bahiapontoba';
+                ?>
+                    <a title="YouTube" style="display: inline-block; vertical-align: top; margin: 0 0 0 8px; text-decoration: none;" href="<?= $youtube; ?>" aria-label="YouTube" class="rede-youtube" target="_blank">
+                        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.546 15.568V8.432L15.818 12l-6.272 3.568z" fill="#FF0000"/>
+                        </svg>
+                    </a>
             </div>
 
             <div class="item" style="padding-right: 10% !important;">
@@ -464,6 +473,16 @@ $ipad = strpos(getUserAgent(), "iPad");
                                     </svg>
                                 </a>
                             <?php endif; ?>
+
+                            <?php
+                            $youtube = get_option('options_youtube');
+                            if (!$youtube) $youtube = 'https://www.youtube.com/@bahiapontoba';
+                            ?>
+                                <a href="<?= $youtube; ?>" aria-label="YouTube" class="rede-youtube" target="_blank">
+                                    <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.546 15.568V8.432L15.818 12l-6.272 3.568z" fill="#FF0000"/>
+                                    </svg>
+                                </a>
 
                         </div>
                         <!-- <a href="<?php bloginfo('url'); ?>/ultimas-noticias" class="bt-ultimas-noticias">
