d0d996336b7127bdf6e4d694ce5abe4d44f6b02d
Albert Cruz <albertcruz@terra.com.br>
2026-06-04 12:21:18 +0100

    SCRUM-46/retirar o "mais noticias" da pagina principal

--- themes/bahia_refactor/sidebar-home2.php
+++ themes/bahia_refactor/sidebar-home2.php
@@ -9,13 +9,4 @@
         </div>
     </div>
     <?php include get_template_directory() . '/ad-long.php'; ?>
-    <div class="box-news-int" style="float: left;">
-        <span class="box-categoria" data-target="cont-box-maislidas">mais notícias</span>
-        <div class="box-maislidos">
-            <div class="cont-tab" id="cont-box-maislidas">
-                <?php mais_noticias_editoria(); ?>
-            </div>
-        </div>
-    </div>
-    <?php include get_template_directory() . '/ad-long.php'; ?>
 </aside>
